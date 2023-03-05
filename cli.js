3bcf7a2d07fc6686aa2b7830808fbb600bf794d6
David <davidcha@email.unc.edu>
2023-03-05 16:08:04 -0500

    implemented basic functionality that needs to be tested

--- cli.js
+++ cli.js
@@ -1,15 +1,13 @@
 #!/usr/bin/env node
 
-import moment from 'moment-timezone';
-import minimist from 'minimist';
-import fetch from 'node-fetch';
-
-const timezone = moment.tz.guess();
+import moment from "moment-timezone";
+import minimist from "minimist";
+import fetch from "node-fetch";
 
 const args = minimist(process.argv.slice(2));
 
+const timezone = args.j || moment.tz.guess();
 console.log(args);
-
 if (args.h) {
   try {
     console.log(`
@@ -23,6 +21,33 @@ if (args.h) {
         `);
     process.exit(0);
   } catch (err) {
-      process.exit(1);
-    }
+    process.exit(1);
+  }
+}
+
+const latitude = (args.n * 1.0 || -1.0 * args.s);
+const longitude = (args.e * 1.0 || -1.0 * args.w);
+console.log(typeof(latitude));
+console.log(typeof(longitude));
+const response = await fetch(
+  `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=precipitation_hours&current_weather=true&timezone=${timezone}`
+);
+const data = await response.json();
+// console.log(data);
+
+// check flags and output desired response
+
+const days = args.d;
+
+if (days == 0) {
+  console.log('today');
+} else if (days > 1) {
+  console.log(`in ${days} days.`);
+} else {
+  console.log("tomorrow");
+}
+
+const json = args.j;
+if (json) {
+  console.log(data);
 }
